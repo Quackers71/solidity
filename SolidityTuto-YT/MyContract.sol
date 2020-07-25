f0ccd4b16c8aa72e4f2eb8f8e176f6df9603844b
Quackers <robq71@gmail.com>
2020-07-25 08:44:41 +0000

    my first smart contract

---
+++ SolidityTuto-YT/MyContract.sol
@@ -0,0 +1,18 @@
+pragma solidity >=0.4.24 <0.7.0;
+
+contract MyContract {
+    string value;
+    
+    constructor() public {
+        value = "myValue";
+    }
+    
+    function get() public view returns(string) {
+        return value;
+    }
+    
+    function set(string _value) public {
+        value = _value;
+    }
+}
+
