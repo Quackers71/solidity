7a32b155130f91798189f00cc970113da114a4c8
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-07-26 10:17:56 +0100

    using enum's for given State's

---
+++ SolidityTuto-YT/MyContract3.sol
@@ -0,0 +1,18 @@
+pragma solidity 0.5.1;
+
+contract MyContract {
+    enum State { Waiting, Ready, Active }
+    State public state;
+    
+    constructor() public {
+        state = State.Waiting;
+    }
+    
+    function activate() public {
+        state = State.Active;
+    }
+    
+    function isActive() public view returns(bool) {
+        return state == State.Active;
+    }
+}
