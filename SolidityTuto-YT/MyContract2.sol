c8340318afd22b61c6249e208582d90e33ac5b3b
Quackers <robq71@gmail.com>
2020-07-26 08:40:35 +0000

    added 2nd Contract still using sol0.5.1

---
+++ SolidityTuto-YT/MyContract2.sol
@@ -0,0 +1,11 @@
+pragma solidity 0.5.1;
+
+contract MyContract {
+    string public constant stringValue = "myString";
+    bool public myBool = true;
+    int public myInt = -1;
+    uint public myUnit = 1;
+    uint8 public myUint8 = 8;
+    uint256 public myUint256 = 99999;
+}
+
