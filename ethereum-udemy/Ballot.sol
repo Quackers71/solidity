6bc454058fe0ba598eb21c47b4846b4e0cba3b29
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-08-03 21:55:15 +0100

    removed duplicate function

--- ethereum-udemy/Ballot.sol
+++ ethereum-udemy/Ballot.sol
@@ -10,8 +10,4 @@ contract Inbox {
     function setMessage(string newMessage) public {
         message = newMessage;
     }
-    
-    function getMessage() public view returns (string) {
-        return message;
-    }
 }
