df1305701eb216edf2c0031f85f4cd6c785fe965
Q <robq71@gmail.com>
2020-08-10 23:47:51 +0100

    trying to run on ubuntu 20.04

--- ethereum-udemy/inbox/compile.js
+++ ethereum-udemy/inbox/compile.js
@@ -2,7 +2,26 @@ const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
 
-const inboxPath = path.resolve(__dirname, 'contracts','Inbox.sol');
-const source = fs.readFileSync(inboxPath, 'utf8');
+const inboxpath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(inboxpath, 'UTF-8');
 
-solc.compile(source, 1);
+var input = {
+    language: 'Solidity',
+    sources: {
+        'Inbox.sol' : {
+            content: source
+        }
+    },
+    settings: {
+        outputSelection: {
+            '*': {
+                '*': [ '*' ]
+            }
+        }
+    }
+};
+
+var output = JSON.parse(solc.compile(JSON.stringify(input)));
+
+exports.abi = output.contracts['Inbox.sol']['Inbox'].abi;
+exports.bytecode = output.contracts['Inbox.sol']['Inbox'].evm.bytecode.object;
